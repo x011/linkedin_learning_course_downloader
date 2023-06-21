821b9ef6ecab8c9f9cd50cdb0a1e075eae810cb9
x011 <x011@users.noreply.github.com>
2023-06-21 12:29:26 +0100

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -32,6 +32,9 @@ base = 'https://www.linkedin.com/learning'
 ctitle = slug.replace('-', ' ').title()
 u = f'{base}/{slug}'
 
+if not os.path.exists(base_path):
+	os.makedirs(base_path)
+
 h = {
 	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0',
 	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
